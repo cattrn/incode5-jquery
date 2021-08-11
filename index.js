24464e00b55b0f953ac93282ff68c954b15c49ae
Caterina Turnbull <caterina.turnbull@gmail.com>
2021-08-12 09:46:33 +1000

    Installs basic dependencies and entry skeleton

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+// might need db connection
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// require routes
+
+
+// body parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// static folder
+app.use(express.static('public'))
+
+// view engine
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+// middleware - routes
+
+
+app.listen(PORT, () => {
+  console.log(`You guys are great! Here's your app: http://localhost:${PORT}`)
+})
