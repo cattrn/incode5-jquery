bc9447ca7ea2509c15be69fd92e491a96a565309
Caterina Turnbull <caterina.turnbull@gmail.com>
2021-08-12 09:54:14 +1000

    Adds home route

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // require routes
-
+const homeRouter = require('./routes/home')
 
 // body parser
 app.use(express.json())
@@ -19,7 +19,7 @@ app.set('view engine', 'ejs')
 app.set('views', './views')
 
 // middleware - routes
-
+app.use('/', homeRouter)
 
 app.listen(PORT, () => {
   console.log(`You guys are great! Here's your app: http://localhost:${PORT}`)
